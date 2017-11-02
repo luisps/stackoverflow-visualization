df9ab7e14794f20fcca337bd10ebe7d44a3955e0
Luis Santos <luis@paiva.eu>
2017-11-02 10:03:00 +0000

    added insert_votes

---
+++ luis/insert_votes.py
@@ -0,0 +1,50 @@
+from global_variables import *
+
+def recreate_table(cur):
+
+    drop_table_query = 'DROP TABLE IF EXISTS Votes'
+    cur.execute(drop_table_query)
+
+    create_table_query = '''\
+CREATE TABLE Votes (
+    Id int8 NOT NULL,
+    PostId int8 NOT NULL,
+    VoteTypeId int2 NOT NULL,
+    CreationDate date NOT NULL,
+    PRIMARY KEY (Id)
+);\
+    '''
+    cur.execute(create_table_query)
+
+def row_process(elem):
+
+    data = (
+            int(elem.attrib['Id']),
+            int(elem.attrib['PostId']),
+            int(elem.attrib['VoteTypeId']),
+            elem.attrib['CreationDate'][:10]
+           )
+
+    return cur.mogrify('(%s,%s,%s,%s)', data)
+
+
+recreate = True
+
+if recreate:
+    recreate_table(cur)
+    conn.commit()
+
+start_time = datetime.now()
+
+xml_file = os.path.join(xml_files_dir, 'Votes.xml')
+insert_query = 'INSERT INTO Votes VALUES '
+
+context = ET.iterparse(xml_file, events=('end',), tag='row')
+fast_iter(context, row_process, conn, cur, insert_query)
+
+conn.commit()
+conn.close()
+
+end_time = datetime.now()
+time_delta = end_time - start_time
+print('XML file:', xml_file, 'Took', time_delta.total_seconds(), 'seconds')
