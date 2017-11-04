3ca6861cefec36cb974964d860a187d3e3275fbd
Ricardo Tavares <rjgtav@hotmail.com>
2017-11-04 18:58:08 +0000

    Hotfix: insert_all wasn't working on Windows

--- luis/insert_all.py
+++ luis/insert_all.py
@@ -1,19 +1,18 @@
 import subprocess
 
 creating_dbs = True
-#regions = ['es']
-regions = ['es', 'pt', 'ru', 'ja']
-#regions = ['en']  #en is the big boy, should run separately
+#regions = ['es', 'pt', 'ru', 'ja']
+regions = ['es']  #en is the big boy, should run separately
 
-if creating_dbs:
-    subprocess.run(['python3', 'create_dbs.py'])
+#if creating_dbs:
+#    subprocess.run(['python3', 'create_dbs.py'])
 
 for region in regions:
     print('Importing files for region:', region)
 
-    subprocess.run(['python3', 'insert_posts.py', region])
-    subprocess.run(['python3', 'insert_votes.py', region])
-    subprocess.run(['python3', 'insert_comments.py', region])
+    subprocess.call(['python3', 'insert_posts.py', region], shell=True)
+    subprocess.call(['python3', 'insert_votes.py', region], shell=True)
+    subprocess.call(['python3', 'insert_comments.py', region], shell=True)
 
     print()
 
