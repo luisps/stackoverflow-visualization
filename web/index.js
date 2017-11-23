412b87d9becaab1e88f6b57e6bde18ab065c8c56
Luis Santos <luis@paiva.eu>
2017-11-23 20:46:49 +0000

    init() and update() functions for graph and heatmap

--- web/index.js
+++ web/index.js
@@ -1,133 +1,29 @@
 window.onload = function () {
 
+
+
     function load_data(region) {
-
-      var dataClusters = {};
-      var dataNodes = {};
-      var dataLinks = {};
-
-      prefix = 'data/' + region + '.stackoverflow.com';
-      console.log('Loading data for region', region);
-
-      d3.csv(prefix + '_clusters.csv', (data) => {
-          data.forEach((row) => {
-              dataClusters['$' + row.tag] = '$' + row.cluster;
-          });
-
-          console.log('Loaded clusters!');
-          d3.csv(prefix + '_community.csv', (data) => {
-              data.forEach((row) => {
-                  let date = row['$date$'].split('-');
-                  let year = +date[0], month = +date[1], day = +date[2];
-
-                  delete row['$date$'];
-                  Object.keys(row).forEach((key) => {
-                      let value = row[key].split('-');
-                      let node = {
-                          tag: key,
-                          answercount: +value[0],
-                          commentcount: +value[1],
-                          questioncount: +value[2],
-                          upvotes: +value[3],
-                          downvotes: +value[4]
-                      };
-
-                      node.id = '$' + key;
-                      node.cluster = dataClusters[node.id];
-
-                      if (node.cluster === undefined)
-                          return;
-
-                      let dataNodesByYear = dataNodes[year] = dataNodes[year] || {};
-                      let dataNodesByMonthAndYear = dataNodesByYear[month] = dataNodesByYear[month] || {};
-                      let dataNodesByDayAndMonthAndYear = dataNodesByMonthAndYear[day] = dataNodesByMonthAndYear[day] || {};
-                      let clusterNode = dataNodesByDayAndMonthAndYear[node.cluster] = dataNodesByDayAndMonthAndYear[node.cluster] || {
-                          children: {},
-                          links: []
-                      };
-
-                      // Process clusters
-                      if (node.id === node.cluster) { // We found the parent
-                          node.children = clusterNode.children;
-                          node.links = clusterNode.links;
-                          dataNodesByDayAndMonthAndYear[node.cluster] = node;
-                      } else {
-                          clusterNode.children[node.id] = node;
-                      }
-                  });
-              });
-
-              console.log('Loaded community!');
-              d3.csv(prefix + '_skills.csv', function (data) {
-                  data.forEach((row) => {
-                      let date = row['$date$'].split('-');
-                      let year = +date[0], month = +date[1];
-
-                      delete row['$date$'];
-                      Object.keys(row).forEach((key) => {
-                          let count = +row[key];
-                          let tag1 = '$' + key.split('$')[0];
-                          let tag2 = '$' + key.split('$')[1];
-
-                          let cluster1 = dataClusters[tag1];
-                          let cluster2 = dataClusters[tag2];
-                          if (cluster1 !== cluster2 && (tag1 !== cluster1 || tag2 !== cluster2))
-                              return;
-
-                          let dataNodesByYear = dataNodes[year];
-                          let dataNodesByMonthAndYear = dataNodesByYear[month];
-                          dataNodesByMonthAndYear = dataNodesByMonthAndYear[Object.keys(dataNodesByMonthAndYear)[0]];
-
-                          if (cluster1 === cluster2) { // Same cluster
-                              let clusterNode = dataNodesByMonthAndYear[cluster1];
-                              clusterNode.links.push({
-                                  source: clusterNode.children[tag1],
-                                  target: clusterNode.children[tag2],
-                                  value: count
-                              });
-                          } else { // Different clusters
-                              let dataLinksByYear = dataLinks[year] = dataLinks[year] || {};
-                              let dataLinksByMonthAndYear = dataLinksByYear[month] = dataLinksByYear[month] || [];
-                              dataLinksByMonthAndYear.push({
-                                  source: dataNodesByMonthAndYear[tag1],
-                                  target: dataNodesByMonthAndYear[tag2],
-                                  value: count
-                              });
-                          }
-                      });
-                  });
-
-                  console.log('Loaded skills!');
-              });
-          });
-      });
-
-      return {dataClusters: dataClusters, dataNodes: dataNodes, dataLinks: dataLinks};
-
     }
 
-
-    var res = load_data('es')
-    dataClusters = res['dataClusters']
-    dataNodes = res['dataNodes']
-    dataLinks = res['dataLinks']
-
-    let d3graph = d3.select('#graph');
-    let d3graphDimensions = window.getComputedStyle(d3graph.node());
-        d3graphDimensions = { height: parseInt(d3graphDimensions.height), width: parseInt(d3graphDimensions.width) };
-    let d3graphNodes, d3graphNodesRadiusScale;
-    let d3graphLinks, d3graphLinksValueScale;
-
-    let d3simulation = d3.forceSimulation()
-        .force('center', d3.forceCenter(d3graphDimensions.width / 2, d3graphDimensions.height / 2))
-        .force('charge', d3.forceManyBody().strength(-300))
-        .force('link', d3.forceLink().id((n) => n.id).distance((l) => 1 / d3graphLinksValueScale(l.value) + 64).strength((l) => d3graphLinksValueScale(l.value)))
-        .force('collision', d3.forceCollide().radius((n) => d3graphNodesRadiusScale(n.answercount + n.questioncount + n.commentcount) + 4))
-    ;
-
-
     function initGraph() {
+
+        d3graph = d3.select('#graph');
+        d3graphDimensions = window.getComputedStyle(d3graph.node());
+            d3graphDimensions = { height: parseInt(d3graphDimensions.height), width: parseInt(d3graphDimensions.width) };
+        //d3graphNodes, d3graphNodesRadiusScale;
+        //var d3graphLinks, d3graphLinksValueScale;
+
+        d3simulation = d3.forceSimulation()
+            .force('center', d3.forceCenter(d3graphDimensions.width / 2, d3graphDimensions.height / 2))
+            .force('charge', d3.forceManyBody().strength(-300))
+            .force('link', d3.forceLink().id((n) => n.id).distance((l) => 1 / d3graphLinksValueScale(l.value) + 64).strength((l) => d3graphLinksValueScale(l.value)))
+            .force('collision', d3.forceCollide().radius((n) => d3graphNodesRadiusScale(n.answercount + n.questioncount + n.commentcount) + 4))
+        ;
+    }
+
+    function updateGraph() {
         // TODO: use the year and month selected from the timeline
+        console.log(dataLinks);
         let links = dataLinks[2017][8];
         let nodes = Object.values(dataNodes[2017][8][1]);
 
@@ -167,6 +63,10 @@ window.onload = function () {
             .text(function (d) { return d.tag })
         ;
 
+        d3graphLinks.exit().remove();
+        d3graphNodes.exit().remove();
+        //d3graphLinks.exit().remove();
+
         function d3simulationOnTick() {
             /*
             d3graphLinks
@@ -188,17 +88,39 @@ window.onload = function () {
 
     function initHeatmap() {
 
-    //setting tag here, eventually tag will be selected on graph
-    var tag = 'javascript';
+        metrics = ['questioncount', 'answercount', 'commentcount']
+        metricNames = ['Question', 'Answer', 'Comment']
+        selectedMetric = 0;
+
+        activity = getActivity(dataNodes, tag)
+
+        heatmap = calendarHeatmap()
+            .selector('.heatmap-container')
+            .tooltipEnabled(true)
+            .colorRange(['#f4f7f7', '#79a8a9'])
+            .onClick(function (data) {
+                console.log('data', data);
+        });
+
+        //Initialize heatmap slider
+        $('.metrics').css('visibility', 'visible');
+        $('.metrics').slick();
+
+        $('.metrics').on('afterChange', function(ev, slick, currentSlide) {
+            selectedMetric = currentSlide;
+            updateHeatmap();
+        });
+
+    }
 
     function getActivity(dataNodes, tag) {
-      activity = []
+        activity = []
 
       for (var year in dataNodes)
         for (var month in dataNodes[year])
           for (var day in dataNodes[year][month]) {
-            tagActivity = dataNodes[year][month][day]['$' + tag];
 
+            tagActivity = dataNodes[year][month][day]['$' + tag];
             activity.push({
               date: new Date(year, month, day),
               questioncount: tagActivity.questioncount,
@@ -211,47 +133,144 @@ window.onload = function () {
     }
 
     function getMetricData(activity, metric) {
-      return activity.map(function(node) {
-        return {
-          date: node.date,
-          count: node[metric]
-        };
-      });
+        return activity.map(function(node) {
+            return {
+                date: node.date,
+                count: node[metric]
+            };
+        });
+    }
+
+    function updateHeatmap() {
+        var metricData = getMetricData(activity, metrics[selectedMetric]);
+        var metricName = metricNames[selectedMetric];
+
+        heatmap.data(metricData);
+        heatmap.tooltipUnit(metricName);
+        heatmap();  // render the chart
     }
 
 
-    var metrics = ['questioncount', 'answercount', 'commentcount']
-    var metricNames = ['Question', 'Answer', 'Comment']
+    /*
+    var res = load_data('es');
+    dataClusters = res['dataClusters'];
+    dataNodes = res['dataNodes'];
+    dataLinks = res['dataLinks'];
+    */
 
-    activity = getActivity(dataNodes, tag)
+    var dataClusters = {};
+    var dataNodes = {};
+    var dataLinks = {};
 
-    var metricData = getMetricData(activity, metrics[0]);
-    var metricName = metricNames[0];
-    var heatmap = calendarHeatmap()
-    .data(metricData)
-    .selector('.heatmap-container')
-    .tooltipEnabled(true)
-    .tooltipUnit(metricName)
-    .colorRange(['#f4f7f7', '#79a8a9'])
-    .onClick(function (data) {
-    console.log('data', data);
+    region = 'es'
+
+    prefix = 'data/' + region + '.stackoverflow.com';
+    console.log('Loading data for region', region);
+
+    d3.csv(prefix + '_clusters.csv', (data) => {
+        data.forEach((row) => {
+            dataClusters['$' + row.tag] = '$' + row.cluster;
+        });
+
+        console.log('Loaded clusters!');
+        d3.csv(prefix + '_community.csv', (data) => {
+            data.forEach((row) => {
+                let date = row['$date$'].split('-');
+                let year = +date[0], month = +date[1], day = +date[2];
+
+                delete row['$date$'];
+                Object.keys(row).forEach((key) => {
+                    let value = row[key].split('-');
+                    let node = {
+                        tag: key,
+                        answercount: +value[0],
+                        commentcount: +value[1],
+                        questioncount: +value[2],
+                        upvotes: +value[3],
+                        downvotes: +value[4]
+                    };
+
+                    node.id = '$' + key;
+                    node.cluster = dataClusters[node.id];
+
+                    if (node.cluster === undefined)
+                        return;
+
+                    let dataNodesByYear = dataNodes[year] = dataNodes[year] || {};
+                    let dataNodesByMonthAndYear = dataNodesByYear[month] = dataNodesByYear[month] || {};
+                    let dataNodesByDayAndMonthAndYear = dataNodesByMonthAndYear[day] = dataNodesByMonthAndYear[day] || {};
+                    let clusterNode = dataNodesByDayAndMonthAndYear[node.cluster] = dataNodesByDayAndMonthAndYear[node.cluster] || {
+                        children: {},
+                        links: []
+                    };
+
+                    // Process clusters
+                    if (node.id === node.cluster) { // We found the parent
+                        node.children = clusterNode.children;
+                        node.links = clusterNode.links;
+                        dataNodesByDayAndMonthAndYear[node.cluster] = node;
+                    } else {
+                        clusterNode.children[node.id] = node;
+                    }
+                });
+            });
+
+            console.log('Loaded community!');
+            d3.csv(prefix + '_skills.csv', function (data) {
+                data.forEach((row) => {
+                    let date = row['$date$'].split('-');
+                    let year = +date[0], month = +date[1];
+
+                    delete row['$date$'];
+                    Object.keys(row).forEach((key) => {
+                        let count = +row[key];
+                        let tag1 = '$' + key.split('$')[0];
+                        let tag2 = '$' + key.split('$')[1];
+
+                        let cluster1 = dataClusters[tag1];
+                        let cluster2 = dataClusters[tag2];
+                        if (cluster1 !== cluster2 && (tag1 !== cluster1 || tag2 !== cluster2))
+                            return;
+
+                        let dataNodesByYear = dataNodes[year];
+                        let dataNodesByMonthAndYear = dataNodesByYear[month];
+                        dataNodesByMonthAndYear = dataNodesByMonthAndYear[Object.keys(dataNodesByMonthAndYear)[0]];
+
+                        if (cluster1 === cluster2) { // Same cluster
+                            let clusterNode = dataNodesByMonthAndYear[cluster1];
+                            clusterNode.links.push({
+                            source: clusterNode.children[tag1],
+                            target: clusterNode.children[tag2],
+                            value: count
+                            });
+                        } else { // Different clusters
+                            let dataLinksByYear = dataLinks[year] = dataLinks[year] || {};
+                            let dataLinksByMonthAndYear = dataLinksByYear[month] = dataLinksByYear[month] || [];
+                            dataLinksByMonthAndYear.push({
+                            source: dataNodesByMonthAndYear[tag1],
+                            target: dataNodesByMonthAndYear[tag2],
+                            value: count
+                            });
+                        }
+                    });
+                });
+
+                console.log('Loaded skills!');
+
+
+                initGraph();
+                updateGraph();
+
+                initHeatmap();
+                updateHeatmap();
+
+
+            });
+        });
     });
 
-    heatmap();  // render the chart
 
-    $('.metrics').on('afterChange', function(ev, slick, currentSlide) {
-      metricData = getMetricData(activity, metrics[currentSlide]);
-      metricName = metricNames[currentSlide];
+}
 
-      heatmap.data(metricData);
-      heatmap.tooltipUnit(metricName);
-      heatmap();
-    });
 
-    $('.metrics').css('visibility', 'visible');
-    $('.metrics').slick();
-
-    }
-
-};
 
